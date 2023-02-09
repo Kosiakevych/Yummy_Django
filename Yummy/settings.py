7c424817e7fb74b8b90e8e7076a277e80106e911
Oleksandr Kosiakevych <oleksandr.kosiakevych@gmail.com>
2023-02-09 12:44:08 +0100

    Update settings.py

--- Yummy/settings.py
+++ Yummy/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#^@kjd#3p20rin4bbqc)khkzem4se1(bu17rki6dq^8@8e5#-l'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
